16d7fc27711e9d7fe35e167839abe1483e1a3e48
SeddikBellamine <bellamine.seddik@gmail.com>
2023-10-06 14:07:21 +0200

    reajust deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,49 +1,28 @@
-import { DeployFunction } from 'hardhat-deploy/types';
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { ethers } from 'hardhat';
 import { saveSmartContractAddress } from '../utils/utils';
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+async function main() {
   console.log('Starting deployment...');
-  const { deployments, getNamedAccounts, ethers } = hre;
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  console.log('deployer: ', deployer);
-  const deploymentResult = await deploy('IExecWhitelist', {
-    from: deployer,
-    log: true,
-  });
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+  // pass the registry instance to the deploy method
+  const IExecWhitelist = await ethers.getContractFactory('IExecWhitelist');
+  const iExecWhitelist = await IExecWhitelist.deploy();
+  const receipt = await iExecWhitelist.deployed();
 
-  if (deploymentResult.newlyDeployed) {
-    console.log(
-      `IExecWhitelist deployed at address: ${deploymentResult.address}`
-    );
-    console.log(
-      `Transaction hash: ${deploymentResult.receipt?.transactionHash}`
-    );
-    console.log(`Gas used: ${deploymentResult.receipt?.gasUsed.toString()}`);
-    const transactionHash = deploymentResult.receipt?.transactionHash;
-    const receipt = await ethers.provider.getTransactionReceipt(
-      transactionHash as string
-    );
-    if (receipt && receipt.status) {
-      console.log('Deployment status: Successful');
-      // save the smart contract address in `.smart-contract-address` file for next usages
-      await saveSmartContractAddress(deploymentResult.address);
-    } else {
-      console.log('Deployment status: Failed');
-    }
-  } else {
-    console.log(
-      'IExecWhitelist already deployed at address:',
-      deploymentResult.address
-    );
-  }
-};
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-func(hre).catch((error) => {
+  console.log(
+    'Gas used: ',
+    (await receipt.deployTransaction.wait()).gasUsed.toString()
+  );
+  // save the smart contract address in `.smart-contract-address` file for next usages
+  await saveSmartContractAddress(iExecWhitelist.address.toString());
+
+  console.log(
+    'IExecWhitelist contract deployed to address:',
+    iExecWhitelist.address
+  );
+}
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
-export default func;
-func.tags = ['IExecWhitelist'];
