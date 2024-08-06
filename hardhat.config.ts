9ea2a8d01151f4d16243672d216e0b81dc004fbf
Abbes Benayache <benayache.abbes@gmail.com>
2024-08-06 10:45:39 +0200

    add bellecour-bubble network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,14 @@ const config: HardhatUserConfig = {
       url: 'https://bellecour.iex.ec',
       accounts: WALLET_PRIVATE_KEY ? [WALLET_PRIVATE_KEY] : [],
     },
+    'bellecour-bubble': {
+      chainId: 65535,
+      gasPrice: 0,
+      blockGasLimit: 6_700_000,
+      hardfork: 'berlin',
+      url: 'http://chain.wp-throughput.az1.internal:8545',
+      accounts: WALLET_PRIVATE_KEY ? [WALLET_PRIVATE_KEY] : [],
+    },
   },
   //to verify contract on Blockscout
   etherscan: {
@@ -38,6 +46,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: 'https://blockscout-v6.bellecour.iex.ec/api',
           browserURL: 'https://blockscout-v6.bellecour.iex.ec',
+        },
+      },
+      {
+        network: 'bubble-bellecour',
+        chainId: 65535,
+        urls: {
+          apiURL: 'http://chain.wp-throughput.az1.internal:4000/api',
+          browserURL: 'http://chain.wp-throughput.az1.internal:4000',
         },
       },
     ],
