4252bdab890d88e3b8acda44721c2a6cec4e43a3
Le-Caignec <robinlecaignec@yahoo.fr>
2025-07-22 10:40:13 +0200

    refactor: simplify solidity configuration by removing unnecessary compilers array

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -72,18 +72,14 @@ const config: HardhatUserConfig = {
     keep: true, // Keep it for slither
   },
   solidity: {
-    compilers: [
-      {
-        version: '0.8.28',
-        settings: {
-          viaIR: true,
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: '0.8.28',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-    ],
+    },
   },
 };
 
